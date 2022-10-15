38c6a01c40b9de380db5585b83d52c6af8e9e62a
Linda Mejia <atenea1205@hotmail.com>
2022-10-15 11:36:12 -0500

    creación de archivos y primeras configuraciones de account new

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,19 @@
+class CategoriesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
