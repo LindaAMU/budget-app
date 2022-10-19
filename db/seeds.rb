66b3cebff713d74bcad8bd41caa8d625cb25db7d
alexandra trivino <alexandratrivino2@gmail.com>
2022-10-18 20:18:15 -0500

    Adding seeds file and adding full_name to Users model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "eliminando instancias"
+
+Account.destroy_all
+Category.destroy_all
+Transaction.destroy_all
+User.destroy_all
+
+
+puts "creando usuarios"
+user1 = User.create(full_name: "Alexandra", email: "alexa@gmail.com", password: "123456")
+user2 = User.create(full_name: "Andres", email: "andres@gmail.com", password: "123456")
+user3 = User.create(full_name: "Mateo", email: "mateo@gmail.com", password: "123456")
+user4 = User.create(full_name: "Daniela", email: "daniela@gmail.com", password: "123456")
+user5 = User.create(full_name: "Linda",  email: "linda@gmail.com", password: "123456")
+
+puts "Usuarios creados"
