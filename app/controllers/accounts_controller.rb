e1979665beeec1a90e2dfce99c517fd97a58ccae
mateo soto <mateo.sotoa@udea.edu.co>
2022-10-21 13:07:56 -0500

    Account Controller

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,4 +1,5 @@
 class AccountsController < ApplicationController
+  before_action :set_account, only: %i[edit update destroy]
   def index
     @accounts = Account.all.where(user_id: current_user.id)
   end
@@ -8,14 +9,35 @@ class AccountsController < ApplicationController
   end
 
   def create
-  end
-
-  def destroy
+    @account = Account.new(account_params)
+    @account.user = current_user
+    if @account.save
+      redirect_to accounts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    @account.update(account_params)
+    redirect_to accounts_path
+  end
+
+  def destroy
+    @account.destroy
+    redirect_to accounts_path, status: :see_other
+  end
+
+  private
+
+  def account_params
+    params.require(:account).permit(:name, :amount, :account_type)
+  end
+
+  def set_account
+    @account = Account.find(params[:id])
   end
 end
