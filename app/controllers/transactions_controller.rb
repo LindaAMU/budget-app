319228eec6e7308f3bcc620e1be76c9b6e189495
Linda Mejia <atenea1205@hotmail.com>
2022-10-21 23:13:47 -0500

    Transacctions en progreso, es necesario realizar cambios en la db

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,20 +1,43 @@
 class TransactionsController < ApplicationController
+  before_action :set_transaction, only: %i[edit update destroy]
+  def index
+    @transactions = Transaction.all.where(user_id: current_user.id)
+  end
+
   def new
     @transaction = Transaction.new(params[:account_id])
   end
 
   def create
+    @transaction = Transaction.new(transaction_params)
+    @transaction.user = current_user
+    @category = Category.find(params[:category_id])
+    @account = Account.find(params[:account_id])
+    @transaction.category = @category
+    @transaction.account = @account
+    if @transaction.save
+      redirect_to transaction_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
-  end
-
-  def index
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def transaction_params
+    params.require(:transaction).permit(:name, :transaction_type)
+  end
+
+  def set_transaction
+    @transaction = Transaction.find(params[:id])
   end
 end
