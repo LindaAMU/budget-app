5957ee063740904bf9d720549a9d44a56192457e
Daniela Calle Meneses <danielacalle08@hotmail.com>
2022-10-24 21:28:06 +0200

    User controller for dashboard render

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
+  resources :users, only: [:show], as: :profile
   resources :accounts, except: %i[show]
   resources :transactions, except: %i[show]
   resources :categories, except: %i[show]
