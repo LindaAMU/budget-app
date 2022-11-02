06a28b63e622d2083ce66b78ec996aa82f60f382
mateo soto <mateo.sotoa@udea.edu.co>
2022-11-01 19:26:57 -0500

    redirect to dashboard when user is login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:full_name])
   end
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || profile_path(current_user)
+  end
 end
