105bccac3f1e06c97b6d73abd83c37bff9a59081
Linda Mejia <atenea1205@hotmail.com>
2022-11-05 15:08:10 -0500

    Se corrige error al cargar los charts al cambiar de pagina

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,4 +5,52 @@ import "bootstrap"
 // import "chartkick/chart.js"
 import "chartkick"
 // import "chartkick/highcharts"
-import "./chart_show"
+// import "./chart_show"
+
+// function clearAllCharts() {
+//   const expensesByCategory = document.getElementById("expenses-by-category");
+//   expensesByCategory.style.display = "none";
+
+//   const myAccounts = document.getElementById("my-accounts");
+//   myAccounts.style.display = "none";
+
+//   const lastSevenDays = document.getElementById("last-seven-days");
+//   lastSevenDays.style.display = "none";
+
+//   const lastMonth = document.getElementById("last-month");
+//   lastMonth.style.display = "none";
+// }
+
+// clearAllCharts();
+// console.log("primer clearAllCharts")
+
+// const chartSelector = document.getElementById("chart-selector")
+// chartSelector.addEventListener("change", (event) => {
+//   console.log("test")
+
+//   switch (event.target.value) {
+//     case 'expenses-by-category':
+//       // clearAllCharts();
+//       const expensesByCategoryMostrar = document.getElementById("expenses-by-category");
+//       expensesByCategoryMostrar.style.display = "block";
+
+//       break;
+//     case 'my-accounts':
+//       // clearAllCharts();
+//       const myAccountsMostrar = document.getElementById("my-accounts");
+//       myAccountsMostrar.style.display = "block";
+//       break;
+//     case 'last-seven-days':
+//       // clearAllCharts();
+//       const lastSevenDaysMostrar = document.getElementById("last-seven-days");
+//       lastSevenDaysMostrar.style.display = "block";
+//       break;
+//     case 'last-month':
+//       // clearAllCharts();
+//       const lastMonthMostrar = document.getElementById("last-month");
+//       lastMonthMostrar.style.display = "block";
+//       break;
+//     default:
+//       // clearAllCharts();
+//   }
+// })
